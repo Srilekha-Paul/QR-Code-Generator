fc246e4c3af035808b0e07a24eeb77fe6031af42
Srilekha Paul <srilekhapaul2003@gmail.com>
2025-06-18 17:15:00 +0530

    end task

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-/*const btnEle =document.querySelector(".btn")
-const imgEle =document.getElementById("QR-img")
-const boxEle = document.getElementById("image-box")
-
-btnEle.addEventListener("click", ()=>{
-    imgEle.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example`
-
-    boxEle.classList.add("show-img")
-})*/
-document.querySelector(".btn").onclick = () => {
-  document.getElementById("QR-img").src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example";
-  document.getElementById("image-box").classList.add("show-img");
+generate.onclick = () => {
+  const val = data.value.trim();
+  if (val) {
+    qr.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${encodeURIComponent(val)}`;
+    qr.style.display = "block";
+  }
+    
 };
